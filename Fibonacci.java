1404ea3e701570bd1e36b031172d9fd1f1ba8ac1
profiot <89318568+profiot@users.noreply.github.com>
2021-08-29 20:06:39 -0400

    added javadoc comments

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,12 +1,19 @@
+
 class Fibonacci{  
+	/**
+	 * @author ian
+	 * @param nth term
+	 * @return long
+	 */
 	public static long fib(long n) {
-	      if ((n == 0) || (n == 1))
-	         return n;
-	      else
-	         return fib(n - 1) + fib(n - 2);
- }    
- public static void main(String args[]){            
-	 long nthTerm = fib(10);
-	 System.out.println(nthTerm);
- }  
+		if ((n == 0) || (n == 1))
+			return n;
+		else
+			return fib(n - 1) + fib(n - 2);
+	}    
+
+	public static void main(String args[]){            
+		long nthTerm = fib(10);
+		System.out.println(nthTerm);
+	}  
 }  
